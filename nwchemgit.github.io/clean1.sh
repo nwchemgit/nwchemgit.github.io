83fea0269790e8b09e326dcfb955e0e8a04698e5
edoapra <edoardo.apra@gmail.com>
2020-07-16 15:22:33 -0800

    Deployed 751bad61a with MkDocs version: 1.1.2

--- nwchemgit.github.io/clean1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -f newa newb
-sed '/130.20./d'  $1 > newa
-sed '/You\ are\ encoura/d'  newa > newb 
-mv newb $1
-rm -f newa
--- nwchemgit.github.io/tut2019/tddft_uvvis/do.bash
+++
@@ -1,1 +0,0 @@
-python nw_spectrum.py -b0.3 -wnm <  h2o-tddft.out
